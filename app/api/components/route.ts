79446a31a9c5c14b96c385ed9f121eb2593d90b9
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-10-17 07:36:23 +0200

    New api for components management.

---
+++ app/api/components/route.ts
@@ -0,0 +1,25 @@
+import {NextResponse} from "next/server";
+import {addDoc, collection, getDocs} from "firebase/firestore";
+import {db} from "@/lib/firebase";
+
+export async function GET() {
+    try {
+        const snapshot = await getDocs(collection(db, 'components'));
+        const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        return NextResponse.json(items, { status: 200 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const data = await request.json();
+        await addDoc(collection(db, 'components'), data); 
+        return NextResponse.json({}, { status: 201 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
