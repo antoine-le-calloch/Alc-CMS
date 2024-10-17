50833b82d9af238618ffd6e303ba9a263c3d7b9d
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-10-17 08:19:31 +0200

    Add PUT and DELETE methode on the API for pages and component management.

--- app/api/pages/route.ts
+++ app/api/pages/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import {addDoc, collection, getDocs} from "firebase/firestore";
+import {addDoc, collection, deleteDoc, doc, getDocs} from "firebase/firestore";
 import {db} from "@/lib/firebase";
 
 export async function GET() {
@@ -18,6 +18,29 @@ export async function POST(request: Request) {
         const data = await request.json();
         await addDoc(collection(db, 'pages'), data); 
         return NextResponse.json({}, { status: 201 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const data = await request.json();
+        const { id, ...rest } = data;
+        await addDoc(collection(db, 'pages'), { id, ...rest });
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: Request) {
+    try {
+        const data = await request.json();
+        await deleteDoc(doc(db, 'pages', data.id));
+        return NextResponse.json({}, { status: 200 });
     } catch (error) {
         let message = error instanceof Error ? error.message : String(error);
         return NextResponse.json({ error: message }, { status: 500 });
