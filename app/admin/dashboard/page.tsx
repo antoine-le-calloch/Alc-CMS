a55ac9ee58661850926cc719cc595f7f4327fab6
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-10-17 07:38:59 +0200

    Improve the dashboard page to display all the pages and components already create.

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,15 +1,60 @@
 "use client";
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {TopBar} from "@/components";
 
 const DashboardPage: React.FC = () => {
+    const [pages, setPages] = useState([])
+    const [components, setComponents] = useState([])
+
+    useEffect(() => {
+        async function fetchPages() {
+            let res = await fetch('/api/pages')
+            let data = await res.json()
+            setPages(data)
+        }
+        async function fetchComponents() {
+            let res = await fetch('/api/components')
+            let data = await res.json()
+            setComponents(data)
+        }
+        fetchPages()
+        fetchComponents()
+    }, [])
+    
     return (
         <div>
             <TopBar titleName={"Dashboard"}/>
-            <div className="container">
-                <div className="p-4">
-                    No content.
+            <div className="container pt-6">
+                <div className="flex justify-around text-center">
+                    <div>
+                        <h1 className="mb-4">
+                            PAGES
+                        </h1>
+                        <div>
+                            {
+                                pages.map((page: any, index: number) => (
+                                    <div key={index} className="border border-gray-200 rounded-lg py-2 px-12 mb-2">
+                                        {page.title}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className="mb-4">
+                            COMPONENTS
+                        </h1>
+                        <div>
+                            {
+                                components.map((component: any, index: number) => (
+                                    <div key={index} className="border border-gray-200 rounded-lg py-2 px-12 mb-2">
+                                        {component.title}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
