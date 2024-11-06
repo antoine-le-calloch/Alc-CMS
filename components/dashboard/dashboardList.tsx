9ff73a0eb6e6e4a10f64be7d97878263153c82c1
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-11-06 04:14:49 +0100

    Creation of components for loading and a list for dashboard page.

---
+++ components/dashboard/dashboardList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {PlusIcon} from "@heroicons/react/24/solid";
+
+interface ItemProps {
+    title: string;
+}
+
+interface DashboardListProps {
+    items: ItemProps[];
+    plusLink: string;
+}
+
+const DashboardList: React.FC<DashboardListProps> = ({items, plusLink}) => {
+    return (
+        <div>
+            <div>
+                {items.length > 0 ?
+                    items.map((item: any, index: number) => (
+                        <div key={index} className="border border-gray-200 rounded-lg py-2 px-12 mb-2">
+                            {item.title}
+                        </div>
+                    )) : <div>No items</div>
+                }
+            </div>
+            <div>
+                <a href={plusLink}>
+                    <PlusIcon className="h-5 w-5 inline-block border border-black rounded-full"/>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardList;
