89cbdc7ab51568357ee1148bfa0068a0d7b86c6f
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-11-11 19:05:47 +0100

    Delete unused code.

--- app/admin/pages/page.tsx
+++ app/admin/pages/page.tsx
@@ -3,11 +3,6 @@
 import React, {useEffect, useState} from 'react';
 import List from "@/components/list";
 
-type Item = {
-    title: string;
-    infos: string;
-};
-
 export default function PagesPage() {
     const [pages, setPages] = useState(null)
 
