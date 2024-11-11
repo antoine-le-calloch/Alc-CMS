815284d62c5c10a7f0a7338d58a3ca8c315e805a
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-11-11 19:33:51 +0100

    Implement the edit page to retrieve the page item and autofill field with them.

--- app/admin/pages/edit/[id]/page.tsx
+++ app/admin/pages/edit/[id]/page.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "@/components/utils/button";
 
 export default function EditPage({ params }: { params: { id: string } }) {
-    const [title, setTitle] = useState("")
-    
+    const [page, setPage] = useState({ title: "" });
+
+    useEffect(() => {
+        async function fetchPage(id: string) {
+            const response = await fetch(`/api/pages/${id}`);
+            if (!response.ok) {throw new Error("An error occurred while fetching the data");}
+            return response.json();
+        }
+        fetchPage(params.id)
+            .then((data) => {
+                setPage(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [params.id])
+
     const handleSubmit = async () => {
         const response = await fetch('/api/pages', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ id: params.id, title })
+            body: JSON.stringify({ id: params.id, title: page.title })
         });
         if (response.ok) {
-            setTitle("");
+            setPage({ title: "" });
             alert("Page successfully edited !");
         }else{
             console.log(response)
@@ -26,15 +41,15 @@ export default function EditPage({ params }: { params: { id: string } }) {
         <div>
             <div className="flex justify-between items-center py-2 mb-4">
                 <h2>
-                    New page creation
+                    Page edit
                 </h2>
             </div>
             <div>
                 <div className="flex flex-col mb-4">
                     <input
                         type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        value={page.title}
+                        onChange={(e) => setPage({ title: e.target.value })}
                         placeholder="Title"
                         className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm 
                         focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
