24d3cde4d50dd139e5528c74a26a2da070f118ac
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-11-22 10:49:18 +0100

    Increase left panel wight and left panel title y gap.

--- components/layout/leftPanel.tsx
+++ components/layout/leftPanel.tsx
@@ -5,12 +5,12 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 
 const LeftPanel = () => {
     return (
-        <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl">
+        <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl w-[180px]">
             <Link href={"/admin"}
                   className="hover:scale-110 duration-700 ease-in-out">
                 <Image src="/logo/logo_ALC.png" alt="Logo" width={70} height={70}/>
             </Link>
-            <div className="flex flex-col items-center gap-4 text-center text-xl mx-2">
+            <div className="flex flex-col items-center gap-y-8 text-center text-xl mx-2">
                 <Link href={"/admin/pages"}
                       className="hover:text-gray-800 duration-500 ease-in-out">
                     PAGES
