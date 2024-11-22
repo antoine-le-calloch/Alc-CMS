46e122b90ade14d8aa37495b546cab201389e8b5
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-11-22 08:36:42 +0100

    Add scale change on hover on the left panel logo.

--- components/layout/leftPanel.tsx
+++ components/layout/leftPanel.tsx
@@ -6,7 +6,8 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 const LeftPanel = () => {
     return (
         <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl">
-            <Link href={"/admin"}>
+            <Link href={"/admin"}
+                  className="hover:scale-110 duration-700 ease-in-out">
                 <Image src="/logo/logo_ALC.png" alt="Logo" width={70} height={70}/>
             </Link>
             <div className="flex flex-col items-center gap-4 text-center text-xl mx-2">
