38a05b70994730fd7b4d947cea0f3d1866b7849c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-02 18:01:45 +0100

    Add link field and plus icon to pageForm. The plus icon will be used to add new block on this page.

--- components/form/pageForm.tsx
+++ components/form/pageForm.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {PlusIcon} from "@heroicons/react/24/solid";
 
 interface PageFormProps {
     page: Page;
@@ -8,32 +9,49 @@ interface PageFormProps {
 const pageForm: React.FC<PageFormProps> = ({page, setPage}) => {
     return (
         <div>
-            <div className="flex flex-col mb-4">
-                <div className="font-bold text-sm">
+            <div className="flex mb-4">
+                <div className="w-1/2">
+                    <div className="font-bold text-sm">
                     Title
+                    </div>
+                    <input
+                        type="text"
+                        value={page.title}
+                        onChange={(e) => setPage({...page, title: e.target.value})}
+                        placeholder="Title"
+                        className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm w-[250px]
+                            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                    />
                 </div>
-                <input
-                    type="text"
-                    value={page.title}
-                    onChange={(e) => setPage({...page, title: e.target.value})}
-                    placeholder="Title"
-                    className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm w-[250px]
-                        focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-                />
+                <div>
+                    <div className="font-bold text-sm">
+                        Link
+                    </div>
+                    <input
+                        type="text"
+                        value={page.link}
+                        onChange={(e) => setPage({...page, link: e.target.value})}
+                        placeholder="Link"
+                        className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm w-[250px]
+                            focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+                    />
+                </div>
             </div>
             <div className="flex flex-col mb-4">
                 <div className="font-bold text-sm">
                     Content
                 </div>
                 <div>
-                    {page.blocks.map((block, index) => (
+                    {page.blocks && page.blocks.map((block, index) => (
                         <div key={index} className="border border-gray-300 rounded-lg p-2 mb-2">
                             {block.name}
                         </div>
                     ))}
                 </div>
-                <div>
-                    
+                <div className="flex justify-center items-center">
+                <PlusIcon className="h-6 w-6 inline-flex border border-black rounded-full 
+                    hover:rotate-[180deg] hover:scale-110 transition duration-500"
+                    />
                 </div>
             </div>
         </div>
