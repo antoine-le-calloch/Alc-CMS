ed34a39edc1c80c3ea102121c4560c148226e61a
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-02 14:50:02 +0100

    Creation of the types.ts file with all types used in the CMS.

---
+++ types.ts
@@ -0,0 +1,17 @@
+// structure of the items used in lists
+interface Item {
+    id: number;
+    title: string;
+}
+
+// structure of the blocks object
+interface Block {
+    name: string;
+    data: any;
+}
+
+// structure of the page object
+interface Page {
+    title: string;
+    blocks: Block[];
+}
