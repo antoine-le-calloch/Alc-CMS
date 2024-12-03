ad9e6bdec133565d06d52286368ad3a74ba2883d
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-03 14:24:38 +0100

    Add id to Page and Block object.

--- components/types/global.ts
+++ components/types/global.ts
@@ -1,11 +1,13 @@
 // structure of the blocks object
 interface Block {
+    id: string;
     name: string;
     data: any;
 }
 
 // structure of the page object
 interface Page {
+    id: string;
     title: string;
     link: string;
     blocks: Block[];
