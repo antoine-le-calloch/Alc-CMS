36e7d29416dc02df40ffaa2e68542b97f6f99d2e
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-03 14:53:02 +0100

    Correct the newPage page to use PageForm with 'null' for the pageToEdit prop.

--- app/admin/pages/new/page.tsx
+++ app/admin/pages/new/page.tsx
@@ -1,49 +1,9 @@
-"use client";
-
-import React, {useState} from 'react';
-import Button from "@/components/utils/button";
+import PageForm from "@/components/form/pageForm";
 
 export default function NewPagePage() {
-    const [title, setTitle] = useState("")
-    
-    const handleSubmit = async () => {
-        const response = await fetch('/api/pages', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: title
-            })
-        });
-        if (response.ok) {
-            setTitle("");
-            alert("New page successfully created !");
-        }else{
-            console.log(response)
-        }
-    }
-    
     return (
         <div>
-            <div>
-                <div className="flex flex-col mb-4">
-                    <div>
-                        Title
-                    </div>
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Title"
-                        className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm 
-                        focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-                    />
-                </div>
-                <Button onClick={handleSubmit} type="submit">
-                    Create
-                </Button>
-            </div>
+            <PageForm pageToEdit={null} />
         </div>
     )
 };
