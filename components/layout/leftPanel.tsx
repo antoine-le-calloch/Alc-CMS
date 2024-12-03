6e5f5365ba9347619cb006b214e3955d8503996e
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-03 11:10:01 +0100

    Change left panel color and change link hover.

--- components/layout/leftPanel.tsx
+++ components/layout/leftPanel.tsx
@@ -5,24 +5,26 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 
 const LeftPanel = () => {
     return (
-        <div className="flex flex-col justify-between items-center h-screen p-4 panel-color shadow-xl w-[180px]">
+        <div className="flex flex-col justify-between items-center bg-slate-900 text-white h-screen p-4 shadow-xl w-[180px]">
             <Link href={"/admin"}
                   className="hover:scale-110 duration-700 ease-in-out">
                 <Image src="/logo/logo_ALC.png" alt="Logo" width={70} height={70}/>
             </Link>
-            <div className="flex flex-col items-center gap-y-8 text-center text-xl mx-2">
+            <div className="flex flex-col items-center gap-y-4 text-center text-xl mx-2 font-bold">
                 <Link href={"/admin/pages"}
-                      className="hover:text-gray-800 duration-500 ease-in-out">
+                      className="border-2 border-transparent rounded-full p-3
+                      hover:border-white duration-500 ease-in-out">
                     PAGES
                 </Link>
                 <Link href={"/admin/blocks"}
-                      className="hover:text-gray-800 duration-500 ease-in-out">
+                      className="border-2 border-transparent rounded-full p-3
+                      hover:border-white duration-500 ease-in-out">
                     BLOCKS
                 </Link>
             </div>
             <div className="h-[70px] flex items-center">
                 <Link href="/">
-                    <Cog6ToothIcon className="size-6"/>
+                    <Cog6ToothIcon className="size-7"/>
                 </Link>
             </div>
         </div>
