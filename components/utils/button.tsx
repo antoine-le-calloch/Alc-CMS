7dd3227e51c4bba6719af5c0a724e261489368b8
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-04 01:26:59 +0100

    Add focus css class to Button component.

--- components/utils/button.tsx
+++ components/utils/button.tsx
@@ -20,8 +20,8 @@ const Button: React.FC<ButtonProps> = ({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md zoom-on-hover 
-                ${className}`}>
+            className={`px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md
+                zoom-on-hover focus ${className}`}>
             {children}
         </button>
     );
