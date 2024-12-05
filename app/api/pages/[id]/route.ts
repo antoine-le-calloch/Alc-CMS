4ce76e0ba8ce670923e15ab55c2cad9728b8605c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-05 01:56:06 +0100

    Move DELETE API to dynamic API route to allow the call with id in url.

--- app/api/pages/[id]/route.ts
+++ app/api/pages/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import {doc, getDoc, setDoc} from "firebase/firestore";
+import {deleteDoc, doc, getDoc, setDoc} from "firebase/firestore";
 import { db } from "@/lib/firebase";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
@@ -30,6 +30,18 @@ export async function PUT(request: Request, { params }: { params: { id: string }
         const data = await request.json();
         const { id: dataId, ...rest } = data;
         await setDoc(doc(db, 'pages', id), rest, { merge: true });
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const { id } = params;
+        await deleteDoc(doc(db, 'pages', id));
         return NextResponse.json({}, { status: 200 });
     } catch (error) {
         let message = error instanceof Error ? error.message : String(error);
