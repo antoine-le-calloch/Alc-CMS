e2e5778bb5ff3c734278c654e44f861c166e832f
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-13 18:37:21 +0100

    Implement API methode for block management.

--- app/api/blocks/[id]/route.ts
+++ app/api/blocks/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { doc, getDoc } from "firebase/firestore";
+import {deleteDoc, doc, getDoc, setDoc} from "firebase/firestore";
 import { db } from "@/lib/firebase";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
@@ -20,6 +20,31 @@ export async function GET(request: Request, { params }: { params: { id: string }
         return NextResponse.json({ id: blockSnap.id, ...blockSnap.data() }, { status: 200 });
     } catch (error) {
         const message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const { id } = params;
+        const data = await request.json();
+        const { id: dataId, ...rest } = data;
+        await setDoc(doc(db, 'blocks', id), rest, { merge: true });
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
+}
+
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const { id } = params;
+        await deleteDoc(doc(db, 'blocks', id));
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        let message = error instanceof Error ? error.message : String(error);
         return NextResponse.json({ error: message }, { status: 500 });
     }
 }
--- app/api/blocks/route.ts
+++ app/api/blocks/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import {addDoc, collection, deleteDoc, doc, getDocs} from "firebase/firestore";
+import {addDoc, collection, getDocs} from "firebase/firestore";
 import {db} from "@/lib/firebase";
 
 export async function GET() {
@@ -16,31 +16,8 @@ export async function GET() {
 export async function POST(request: Request) {
     try {
         const data = await request.json();
-        await addDoc(collection(db, 'blocks'), data); 
+        await addDoc(collection(db, 'blocks'), data);
         return NextResponse.json({}, { status: 201 });
-    } catch (error) {
-        let message = error instanceof Error ? error.message : String(error);
-        return NextResponse.json({ error: message }, { status: 500 });
-    }
-}
-
-export async function PUT(request: Request) {
-    try {
-        const data = await request.json();
-        const { id, ...rest } = data;
-        await addDoc(collection(db, 'blocks'), { id, ...rest });
-        return NextResponse.json({}, { status: 200 });
-    } catch (error) {
-        let message = error instanceof Error ? error.message : String(error);
-        return NextResponse.json({ error: message }, { status: 500 });
-    }
-}
-
-export async function DELETE(request: Request) {
-    try {
-        const data = await request.json();
-        await deleteDoc(doc(db, 'blocks', data.id));
-        return NextResponse.json({}, { status: 200 });
     } catch (error) {
         let message = error instanceof Error ? error.message : String(error);
         return NextResponse.json({ error: message }, { status: 500 });
