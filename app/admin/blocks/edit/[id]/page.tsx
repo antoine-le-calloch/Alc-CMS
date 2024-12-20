d27d5b683087a6b233617b7940bdcaf7490af811
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-20 19:45:10 +0100

    Add page to edit page and block name.

--- app/admin/blocks/edit/[id]/page.tsx
+++ app/admin/blocks/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
 import BlockForm from "@/components/form/BlockForm";
 
-export default async function EditBlock({ params }: { params: { id: string } }) {
+export default async function EditBlockPage({ params }: { params: { id: string } }) {
     let data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blocks/${params.id}`)
     let blockToEdit = await data.json()
     
--- app/admin/pages/edit/[id]/page.tsx
+++ app/admin/pages/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
 import PageForm from "@/components/form/PageForm";
 
-export default async function EditPage({ params }: { params: { id: string } }) {
+export default async function EditPagePage({ params }: { params: { id: string } }) {
     let data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pages/${params.id}`)
     let pageToEdit = await data.json()
     
