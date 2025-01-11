69c22ff0b7797d07285f2922e102a101d21b9a8e
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-01-11 13:52:29 +0100

    Correct bad new page layout naming and create the layout for the new block page.

--- app/admin/blocks/new/layout.tsx
+++ app/admin/blocks/new/layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TopBar from "@/components/layout/TopBar";
 
-export default function NewPageLayout({children}: {
+export default function NewBlockLayout({children}: {
     children: React.ReactNode
 }) {
     return (
         <section>
-            <TopBar titleName={"Page creation"}/>
+            <TopBar titleName={"Block creation"}/>
             <div className="container p-4">
                 {children}
             </div>
