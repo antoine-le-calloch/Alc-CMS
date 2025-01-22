fe97297e0c5b70ce349247f2a0f60f47873cdc65
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-01-21 23:25:28 -0600

    Add a home link to the left panel.

--- components/layout/LeftPanel.tsx
+++ components/layout/LeftPanel.tsx
@@ -11,6 +11,11 @@ const LeftPanel = () => {
                 <Image src="/logo/logo_alc.png" alt="Logo" width={70} height={70}/>
             </Link>
             <div className="flex flex-col items-center gap-y-4 text-center text-xl mx-2 font-bold">
+                <Link href={"/admin/"}
+                      className="border-2 border-transparent rounded-full p-3
+                      hover:border-white duration-500 ease-in-out">
+                    HOME
+                </Link>
                 <Link href={"/admin/pages"}
                       className="border-2 border-transparent rounded-full p-3
                       hover:border-white duration-500 ease-in-out">
