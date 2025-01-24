efb9d3b041f78d283895c4a3f0429304dc42f021
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-01-23 19:23:23 -0600

    Rename every variable from item to block in Blocklist as we don't use this component as a generic one anymore.

--- components/dashboard/BlockList.tsx
+++ components/dashboard/BlockList.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import {PlusIcon} from "@heroicons/react/16/solid";
 
 interface BlockListProps {
-    blocks: Item[];
+    blocks: Block[];
     editBlockLink: string;
     addBlockLink: string;
 }
@@ -17,11 +17,11 @@ const BlockList: React.FC<BlockListProps> = ({blocks, editBlockLink, addBlockLin
     return (
         <div>
             <div className="flex flex-col">
-                {blocks.length > 0 ? blocks.map((item: Item) => (
-                        <Link key={item.id} href={`/admin/${editBlockLink}/${item.id}`} draggable={true} 
-                              onDragStart={(e) => handleDragStart(e, item.id)}
+                {blocks.length > 0 ? blocks.map((block: Block) => (
+                        <Link key={block.id} href={`/admin/${editBlockLink}/${block.id}`} draggable={true} 
+                              onDragStart={(e) => handleDragStart(e, block.title)}
                               className="bg-gray-100 border border-gray-300 rounded-lg py-2 px-12 mb-2 zoom-on-hover">
-                            {item.title}
+                            {block.title}
                         </Link>
                 )) : (
                     <div className="text-gray-500 bg-gray-100 opacity-50 border border-gray-500 rounded-lg py-2 px-12 mb-2 border-dashed">
