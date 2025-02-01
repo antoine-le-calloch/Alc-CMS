e0058ac4c5da8573c5718fc65f5a18916eb314f2
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-01 02:16:55 -0600

    Improve the preview index choose by using item dragover information.

--- components/dashboard/pages/PageItemBlockList.tsx
+++ components/dashboard/pages/PageItemBlockList.tsx
@@ -6,23 +6,30 @@ interface PageItemBlockLIstProps {
 }
 
 const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page}) => {
-    const [previewIndex, setPreviewIndex] = useState<number | null>(null);
+    const [previewIndex, setPreviewIndex] = useState<number>(0);
     const [blockDragged, setBlockDragged] = useState<Block | null>(null);
-
-    const onDragOver = (e: any) => {
+    
+    const onDragEnter = (e: any) => {
         e.preventDefault();
-        const { top, bottom } = e.target.getBoundingClientRect();
-        const middle = (top + bottom) / 2;
-        const offset = e.clientY - middle;
-        
+        setBlockDragged({
+            id: e.dataTransfer.getData("blockId"),
+            title: e.dataTransfer.getData("blockTitle")
+        });
+        console.log(blockDragged);
+    }
+    
+    const onDragOver = (e: any, index: number) => {
+        e.preventDefault();
         if (!blockDragged){
             setBlockDragged({
                 id: e.dataTransfer.getData("blockId"),
                 title: e.dataTransfer.getData("blockTitle")
-            })
+            });
         }
-        
-        setPreviewIndex(offset > 0 ? page.blocks.length - 1 : 0);
+        if (previewIndex !== index) {
+            console.log(index);
+            setPreviewIndex(index);
+        }
     }
     
     const onDrop = async (e: React.DragEvent<HTMLDivElement>) => {
@@ -36,7 +43,7 @@ const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page}) => {
                 console.error("Error:", error);
                 alert(error.message);
             }
-            setPreviewIndex(null);
+            setBlockDragged(null);
         }
     }
     
@@ -48,8 +55,8 @@ const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page}) => {
     }
 
     return (
-        <div className="flex flex-col items-center mt-2" onDragOver={onDragOver} onDrop={(e) => onDrop(e)}
-         onDragLeave={() => setPreviewIndex(null)}>
+        <div className="flex flex-col items-center mt-2" onDrop={(e) => onDrop(e)}
+             onDragEnter={(e => onDragEnter(e))} onDragExit={() => setBlockDragged(null)}>
             {page.blocks.length > 0 ? page.blocks.map((block: Block, index: number) => (
                 <div key={index}>
                     {blockDragged && previewIndex === index &&
@@ -57,7 +64,8 @@ const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page}) => {
                             {blockDragged.title}
                         </div>
                     }
-                    <div draggable={true} onDragStart={(e) => onDragStart(e, index)} key={index} 
+                    <div draggable={true}  key={index}
+                         onDragOver={(e) => onDragOver(e, index)} onDragStart={(e) => onDragStart(e, index)}
                          className="bg-gray-100 border border-gray-300 rounded-lg py-2 px-12 mb-2 cursor-grab hover:scale-105 duration-200 relative">
                         <div className="absolute left-1 top-0.5 text-xs text-gray-400">{index}</div>
                         {block.title}
