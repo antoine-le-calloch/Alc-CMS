3de96a7ab3736c16ab7804dddcd57110041e4941
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-01 00:04:51 -0600

    Move BlockList and PageList a folder different for each list.

---
+++ components/dashboard/pages/PageList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {PlusIcon} from "@heroicons/react/16/solid";
+import PageItem from "@/components/dashboard/pages/PageItem";
+
+interface PageListProps {
+    pages: Page[];
+    editPageLink: string;
+    addPageLink: string;
+    blocks: Block[] | null;
+}
+
+const PageList: React.FC<PageListProps> = ({pages, editPageLink, addPageLink, blocks}) => {
+    return (
+        <div>
+            <div className="flex flex-col">
+                {pages.length > 0 ? pages.map((page: Page) => (
+                    <PageItem pages={pages} page={page} editPageLink={editPageLink} blocks={blocks} key={page.id}/>
+                )): (
+                    <div className="text-gray-500 bg-gray-100 opacity-50 border border-gray-500 rounded-lg py-2 px-12 mb-2 border-dashed">
+                        No pages
+                    </div>
+                )}
+            </div>
+            <div className="py-1">
+                <a href={`/admin/${addPageLink}`}>
+                    <PlusIcon className="plus-icon-style h-6 w-6"/>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default PageList;
