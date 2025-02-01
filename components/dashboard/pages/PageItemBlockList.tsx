75559963be6ff316905954edff8617bdfd4fe786
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-01 00:31:06 -0600

    Create the PageItemBlockList component to display the list of blocks into each page on the dashboard, and that allows the drag and drop system.

---
+++ components/dashboard/pages/PageItemBlockList.tsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import {savePage} from "@/components/services/SavePage";
+
+interface PageItemBlockLIstProps {
+    page: Page;
+}
+
+const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page}) => {
+    const [previewIndex, setPreviewIndex] = useState<number | null>(null);
+    const [blocks, setBlocks] = useState<Block[]>(page.blocks);
+
+    const onDragOver = (e: any) => {
+        e.preventDefault();
+        const { top, bottom } = e.target.getBoundingClientRect();
+        const middle = (top + bottom) / 2;
+        const offset = e.clientY - middle;
+        
+        if (!previewIndex) {
+            const blockToAdd = {id: e.dataTransfer.getData("blockId"), title: e.dataTransfer.getData("blockTitle")};
+            if (blockToAdd){
+                setPreviewIndex(offset > 0 ? blocks.length : 0);
+                setBlocks(previewIndex === 1 ? [...blocks, blockToAdd] : [blockToAdd, ...blocks]);
+            }
+        }else if (offset !== previewIndex) {
+            const blockToAdd = blocks[previewIndex];
+            blocks[previewIndex] = blocks[offset > 0 ? blocks.length - 1 : 0]
+            setPreviewIndex(offset > 0 ? blocks.length - 1 : 0);
+            blocks[previewIndex] = blockToAdd;
+        }
+    }
+    
+    const onDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        if (page.blocks.length === blocks.length + 1) {
+            try {
+                await savePage({...page, blocks: blocks}, true);
+                window.location.href = '/admin/';
+            } catch (error: any) {
+                console.error("Error:", error);
+                alert(error.message);
+            }
+        }
+    }
+    
+    const onDragStart = async ( e: React.DragEvent<HTMLDivElement>, index: number) => {
+        if (page.id) {
+            e.dataTransfer.setData("pageId", page.id);
+            e.dataTransfer.setData("blockIndex", index.toString());
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center mt-2" onDragOver={onDragOver} onDrop={(e) => onDrop(e)}>
+            {page.blocks.length > 0 ? page.blocks.map((block: Block, index: number) => (
+                <div draggable={true} onDragStart={(e) => onDragStart(e, index)} key={index} 
+                     className="bg-gray-100 border border-gray-300 rounded-lg py-2 px-12 mb-2 cursor-grab hover:scale-105 duration-200 relative">
+                    <div className="absolute left-1 top-0.5 text-xs text-gray-400">{index}</div>
+                    {block.title}
+                </div>
+            )) : (
+                <div className="text-gray-500 bg-gray-100 opacity-50 border border-gray-500 rounded-lg py-2 px-12 mb-2 border-dashed">
+                    No blocks
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PageItemBlockLIst;
