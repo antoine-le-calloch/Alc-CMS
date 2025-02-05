bfb5e1cba70d4dd34d41e9800e8ba042f9a20da8
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-05 00:01:20 -0600

    Improve the left panel by using a fixed div so the panel follows the user screen and by increasing its size on hover.

--- components/layout/LeftPanel.tsx
+++ components/layout/LeftPanel.tsx
@@ -5,32 +5,37 @@ import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 
 const LeftPanel = () => {
     return (
-        <div className="flex flex-col justify-between items-center bg-slate-900 text-white dark:bg-white dark:text-slate-900 p-4 shadow-xl w-[180px]">
-            <Link href={"/admin"}
-                  className="hover:scale-110 duration-700 ease-in-out">
-                <Image src="/logo/logo_alc.png" alt="Logo" width={70} height={70}/>
-            </Link>
-            <div className="flex flex-col items-center gap-y-4 text-center text-xl mx-2 font-bold">
-                <Link href={"/admin/"}
-                      className="border-2 border-transparent rounded-full p-3
-                      hover:border-white duration-500 ease-in-out">
-                    HOME
-                </Link>
-                <Link href={"/admin/pages"}
-                      className="border-2 border-transparent rounded-full p-3
-                      hover:border-white duration-500 ease-in-out">
-                    PAGES
-                </Link>
-                <Link href={"/admin/blocks"}
-                      className="border-2 border-transparent rounded-full p-3
-                      hover:border-white duration-500 ease-in-out">
-                    BLOCKS
-                </Link>
-            </div>
-            <div className="h-[70px] flex items-center">
-                <Link href={"/admin/settings"}>
-                    <Cog6ToothIcon className="size-7 rotate-on-hover"/>
-                </Link>
+        <div className="relative min-w-40 h-screen">
+            <div className="fixed shadow-xl w-40 h-screen hover:w-48 duration-500 ease-in-out
+            bg-slate-900 text-white dark:bg-white dark:text-slate-900">
+                <div className="flex flex-col justify-between items-center p-4 h-full">
+                    <Link href={"/admin"}
+                          className="hover:scale-110 duration-700 ease-in-out">
+                        <Image src="/logo/logo_alc.png" alt="Logo" width={70} height={70}/>
+                    </Link>
+                    <div className="flex flex-col items-center gap-y-4 text-center text-xl mx-2 font-bold">
+                        <Link href={"/admin/"}
+                              className="border-2 border-transparent rounded-full p-3
+                              hover:border-white duration-500 ease-in-out">
+                            HOME
+                        </Link>
+                        <Link href={"/admin/pages"}
+                              className="border-2 border-transparent rounded-full p-3
+                              hover:border-white duration-500 ease-in-out">
+                            PAGES
+                        </Link>
+                        <Link href={"/admin/blocks"}
+                              className="border-2 border-transparent rounded-full p-3
+                              hover:border-white duration-500 ease-in-out">
+                            BLOCKS
+                        </Link>
+                    </div>
+                    <div className="h-[70px] flex items-center">
+                        <Link href={"/admin/settings"}>
+                            <Cog6ToothIcon className="size-7 rotate-on-hover"/>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
