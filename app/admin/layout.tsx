11edd1c0e03fa5c25367e8be01dab20552e441ee
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-04 23:33:34 -0600

    Use min-h-screen instead.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,7 +5,7 @@ export default function AdminLayout({children}: {
     children: React.ReactNode
 }) {
     return (
-        <div className="flex">
+        <div className="flex min-h-screen">
             <LeftPanel/>
             <div className="flex flex-col w-full bg-[#e9e7e2]">
                 {children}
