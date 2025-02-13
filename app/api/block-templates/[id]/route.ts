68a860a1938c6fec37cdc890413f27e28dced4bf
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-12 23:19:18 -0600

    Rename block to blockTemplate.

--- app/api/block-templates/[id]/route.ts
+++ app/api/block-templates/[id]/route.ts
@@ -10,14 +10,14 @@ export async function GET(request: Request, { params }: { params: { id: string }
             return NextResponse.json({ error: "ID is required" }, { status: 400 });
         }
 
-        const blockRef = doc(db, 'blocks', id);
-        const blockSnap = await getDoc(blockRef);
+        const blockTemplateRef = doc(db, 'blockTemplates', id);
+        const blockTemplateSnap = await getDoc(blockTemplateRef);
 
-        if (!blockSnap.exists()) {
+        if (!blockTemplateSnap.exists()) {
             return NextResponse.json({ error: "Block not found" }, { status: 404 });
         }
 
-        return NextResponse.json({ id: blockSnap.id, variables: blockSnap.data(), ...blockSnap.data() }, { status: 200 });
+        return NextResponse.json({ id: blockTemplateSnap.id, variables: blockTemplateSnap.data(), ...blockTemplateSnap.data() }, { status: 200 });
     } catch (error) {
         const message = error instanceof Error ? error.message : String(error);
         return NextResponse.json({ error: message }, { status: 500 });
@@ -29,7 +29,7 @@ export async function PUT(request: Request, { params }: { params: { id: string }
         const { id } = params;
         const data = await request.json();
         const { id: dataId, ...rest } = data;
-        await setDoc(doc(db, 'blocks', id), rest, { merge: true });
+        await setDoc(doc(db, 'blockTemplates', id), rest, { merge: true });
         return NextResponse.json({}, { status: 200 });
     } catch (error) {
         let message = error instanceof Error ? error.message : String(error);
@@ -41,7 +41,7 @@ export async function PUT(request: Request, { params }: { params: { id: string }
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
     try {
         const { id } = params;
-        await deleteDoc(doc(db, 'blocks', id));
+        await deleteDoc(doc(db, 'blockTemplates', id));
         return NextResponse.json({}, { status: 200 });
     } catch (error) {
         let message = error instanceof Error ? error.message : String(error);
