774f2ebe7638b73a19ca939867951325cdcdba92
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-22 20:10:37 -0600

    Pass blocks prop to PageItem.

--- components/dashboard/pages/PageList.tsx
+++ components/dashboard/pages/PageList.tsx
@@ -4,16 +4,17 @@ import PageItem from "@/components/dashboard/pages/PageItem";
 
 interface PageListProps {
     pages: Page[];
+    blocks: Block[];
     editPageLink: string;
     addPageLink: string;
 }
 
-const PageList: React.FC<PageListProps> = ({pages, editPageLink, addPageLink}) => {
+const PageList: React.FC<PageListProps> = ({pages, blocks, editPageLink, addPageLink}) => {
     return (
         <div className="min-w-80">
             <div className="flex flex-col">
                 {pages.length > 0 ? pages.map((page: Page) => (
-                    <PageItem page={page} editPageLink={editPageLink} key={page.id}/>
+                    <PageItem page={page} blocks={blocks} editPageLink={editPageLink} key={page.id}/>
                 )): (
                     <div className="text-gray-500 bg-gray-100 opacity-50 border border-gray-500 rounded-lg py-4 px-12 mb-2 border-dashed">
                         No pages
