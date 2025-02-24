2e0a62c300bd84b97bfb2b64977b4693553e675c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-02-23 19:57:27 -0600

    Update onDragStart to use itemId instead of index in PageItemBlockList

--- components/dashboard/pages/PageItemBlockList.tsx
+++ components/dashboard/pages/PageItemBlockList.tsx
@@ -57,10 +57,10 @@ const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page, blocks}) => 
         }
     }
     
-    const onDragStart = async ( e: React.DragEvent<HTMLDivElement>, index: number) => {
+    const onDragStart = async ( e: React.DragEvent<HTMLDivElement>, itemId: string) => {
         if (page.id) {
             e.dataTransfer.setData("pageId", page.id);
-            e.dataTransfer.setData("blockIndex", index.toString());
+            e.dataTransfer.setData("itemId", itemId);
         }
     }
 
@@ -75,9 +75,9 @@ const PageItemBlockLIst: React.FC<PageItemBlockLIstProps> = ({page, blocks}) => 
                             {titleDragged}
                         </div>
                     }
-                    <div draggable={true}  key={index}
+                    <div draggable={item.id != null}  key={index}
                          onDragOver={(e) => onDragOver(e, index)} 
-                         onDragStart={(e) => onDragStart(e, index)}
+                         onDragStart={(e) => {if (item.id != null) onDragStart(e, item.id)}}
                          className="bg-gray-100 border border-gray-300 rounded-lg py-4 mb-2 cursor-grab zoom-on-hover duration-200 relative">
                         <div className="absolute left-1 top-0.5 text-xs text-gray-400">{index}</div>
                         {blocks.find((block: Block) => block.id === item.blockId)?.title}
