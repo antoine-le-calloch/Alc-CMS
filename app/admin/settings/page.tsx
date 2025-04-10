2a1516b926793d74116ac8e6d510ef9622d6ded0
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-04-09 21:36:26 -0500

    Add SettingsPage component with TopBar for CMS settings.

---
+++ app/admin/settings/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+import TopBar from "@/components/layout/TopBar"
+
+export default function SettingsPage() {
+    return (
+        <div>
+            <TopBar titleName="Settings" />
+        </div>
+    )
+};
