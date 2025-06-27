45dca33062647818296da2ca71c43e2057060d93
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-06-26 20:14:36 -0500

    Update TopBar component to adjust padding and improve layout consistency.

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -7,7 +7,7 @@ interface TopBarProps {
 const TopBar: React.FC<TopBarProps> = ({titleName}) => {
     return (
         <div className="relative h-20">
-            <div className="fixed border-b w-full shadow-md h-20 flex items-center justify-left px-4 z-20
+            <div className="fixed left-36 right-0 border-b h-20 shadow-md flex items-center justify-left z-20 px-6
             border-gray-200 bg-white">
                 <h1 className="font-bold uppercase">
                     {titleName}
